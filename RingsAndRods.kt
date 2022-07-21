7d6b4520a0ca0ceb36a9f0a34c8d7fe900946136
Viacheslav Veselov <vycheslav5veselov@gmail.com>
2022-07-21 21:46:40 +0300

    Update RingsAndRods.kt

--- RingsAndRods.kt
+++ RingsAndRods.kt
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/rings-and-rods/submissions/
+// https://leetcode.com/problems/rings-and-rods/
 // There are n rings and each ring is either red, green, or blue. The rings are distributed across ten rods labeled from 0 to 9. 
 // You are given a string rings of length 2n that describes the n rings that are placed onto the rods. Every two characters in rings forms a color-position pair 
 // that is used to describe each ring where: 
