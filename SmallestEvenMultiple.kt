8338cb238a33c56589143ca2061a0d739bdc1f31
Viacheslav Veselov <vycheslav5veselov@gmail.com>
2023-01-14 17:47:34 +0200

    Create SmallestEvenMultiple.kt

---
+++ SmallestEvenMultiple.kt
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/smallest-even-multiple/description/
+
+// Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+
+class Solution {
+    fun smallestEvenMultiple(n: Int): Int {
+        var n1 = n
+
+        while (n1 % 2 != 0 || n1 % n != 0) {
+            n1++
+        }
+
+        return n1
+    }
+}
