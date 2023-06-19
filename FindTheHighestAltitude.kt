44e401fca1677271dd60faa6aeda434445747e7d
Viacheslav Veselov <vycheslav5veselov@gmail.com>
2023-06-19 09:12:51 +0300

    Create FindTheHighestAltitude.kt

---
+++ FindTheHighestAltitude.kt
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/find-the-highest-altitude/description/
+// There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
+// You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). 
+// Return the highest altitude of a point.
+
+class Solution {
+    fun largestAltitude(gain: IntArray): Int {
+        var altitudes = intArrayOf()
+        altitudes += 0
+        var max = 0
+
+        gain.forEachIndexed { index, int ->
+            val newAltitude = altitudes.get(index) + int
+            altitudes += newAltitude
+            if (newAltitude > max) max = newAltitude
+        }
+
+        return max
+    }
+}
