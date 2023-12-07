d5c2b85aebe3bba29bb60cae4b218dde040080cc
Viacheslav Veselov <vycheslav5veselov@gmail.com>
2023-12-07 21:45:48 +0200

    Create CalculateMoneyInLeetcodeBank.kt

---
+++ CalculateMoneyInLeetcodeBank.kt
@@ -0,0 +1,32 @@
+// leetcode.com/problems/calculate-money-in-leetcode-bank/description/
+// Hercy wants to save money for his first car. He puts money in the Leetcode bank every day.
+// He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday, he will put in $1 more than the day before. On every subsequent Monday, 
+// he will put in $1 more than the previous Monday.
+// Given n, return the total amount of money he will have in the Leetcode bank at the end of the nth day.
+
+class Solution {
+    fun totalMoney(n: Int): Int {
+        var days = n
+        var daysToMonday = 0
+        var putInPreviousMonday = 0
+        var putInPreviousDay = 0
+        var result = 0
+
+        while (days != 0) {
+            if (daysToMonday == 0) {
+                result += putInPreviousMonday + 1
+                putInPreviousMonday = putInPreviousMonday + 1
+                putInPreviousDay = putInPreviousMonday
+                daysToMonday += 6
+            } else {
+                result += putInPreviousDay + 1
+                putInPreviousDay = putInPreviousDay + 1
+                daysToMonday--
+            }
+
+            days--
+        }
+
+        return result
+    }
+}
