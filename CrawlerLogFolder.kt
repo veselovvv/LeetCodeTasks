398fa7578f9c3ac61b8ec566170b3271bf3c71c2
Viacheslav Veselov <vycheslav5veselov@gmail.com>
2024-07-11 15:51:48 +0300

    Create CrawlerLogFolder.kt

---
+++ CrawlerLogFolder.kt
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/crawler-log-folder/description/
+// The Leetcode file system keeps a log each time some user performs a change folder operation.
+// The operations are described below:
+// "../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
+// "./" : Remain in the same folder.
+// "x/" : Move to the child folder named x (This folder is guaranteed to always exist).
+// You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+// The file system starts in the main folder, then the operations in logs are performed.
+//  Return the minimum number of operations needed to go back to the main folder after the change folder operations.
+
+class Solution {
+    fun minOperations(logs: Array<String>): Int {
+        var currentDir = "/"
+        
+        logs.forEach { log ->
+            when (log) {
+                "../" -> {
+                    if (currentDir != "/") 
+                        currentDir = moveBackToOneDir(currentDir)
+                }
+                "./" -> {}
+                else -> {
+                    currentDir = log + currentDir
+                }
+            }
+        }
+
+        var result = 0
+
+        while (currentDir != "/") {
+            currentDir = moveBackToOneDir(currentDir)
+            result++
+        }
+
+        return result
+    }
+
+    fun moveBackToOneDir(currentDir: String): String {
+        var newDir = ""
+        var isSlashReached = false
+
+        currentDir.forEach { char ->
+            if (isSlashReached) 
+                newDir += char
+            else if (char == '/')
+                isSlashReached = true
+        }
+
+        return newDir
+    }
+}
